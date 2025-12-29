9985a890f6de843f09ff81bd306818f7a1dad11f
Cass <cassandre.canvot@gmail.com>
2025-12-29 19:39:34 +0100

    Update parking.h

--- include/parking.h
+++ include/parking.h
@@ -1,23 +1,27 @@
 #ifndef PARKING_H
 #define PARKING_H
 
-#define TOTAL_SPOTS 42 // ici tu peux changer si tu change le nombre de P
+#define TOTAL_SPOTS 42 
 
 typedef struct
 {
     int screen_x;
     int screen_y;
     int is_occupied;
+    int type_vehicule;
 } ParkingSpot;
 
+// Variable globale partagée
 extern ParkingSpot all_spots[TOTAL_SPOTS];
 
+// Prototypes des fonctions (Juste les noms, pas le code)
 void display_static_map(const char *filename);
 void goto_xy(int x, int y);
-
 void init_spots_from_map(const char *filename);
-
 void draw_spot(ParkingSpot spot, int is_selected);
 void draw_all_spots(int selected_index);
+
+// On ajoute celle-ci pour que le main la connaisse
+void init_modeles(); 
 
 #endif
