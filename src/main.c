d080f87701a52f89266c885bb90855cd6d3f7937
Cass <cassandre.canvot@gmail.com>
2025-12-30 21:56:06 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ char key_pressed()
 {
     struct termios oldterm, newterm;
     int oldfd;
-    int c; // <- ICI : int, plus char
+    int c; 
     char result = 0;
 
     tcgetattr(STDIN_FILENO, &oldterm);
@@ -37,6 +37,9 @@ char key_pressed()
 
 int main()
 {
+    // --- 1. IMPORTANT : On charge les dessins des véhicules ici ---
+    init_modeles(); 
+
     const char *map_path = "assets/parking_map.txt";
 
     display_static_map(map_path);
@@ -45,9 +48,8 @@ int main()
     int selected_spot_index = 0;
     draw_all_spots(selected_spot_index);
 
-    // CHANGEMENT: Afficher les messages plus bas
-    goto_xy(0, 14); // Anciennement 10
-    // printf("Utilisez ZQSD pour déplacer, ESPACE pour changer l'état, 'e' pour quitter.\n");
+    goto_xy(0, 14); 
+    printf("ZQSD: Deplacer | ESPACE: Changer vehicule | E: Quitter\n");
 
     char key = 0;
     while (key != 'e')
@@ -59,8 +61,6 @@ int main()
             int old_selection = selected_spot_index;
             int selection_changed = 0;
 
-            // --- CHANGEMENT DE LA LOGIQUE ZQSD ---
-            // La grille est maintenant de 8 colonnes x 4 rangées
             switch (key)
             {
             case 'q': // gauche
@@ -79,7 +79,7 @@ int main()
                 }
                 break;
 
-            case 'z': // haut => recule de 1
+            case 'z': // haut
                 if (selected_spot_index > 0)
                 {
                     selected_spot_index--;
@@ -87,7 +87,7 @@ int main()
                 }
                 break;
 
-            case 's': // bas => avance de 1
+            case 's': // bas
                 if (selected_spot_index < TOTAL_SPOTS - 1)
                 {
                     selected_spot_index++;
@@ -95,28 +95,44 @@ int main()
                 }
                 break;
 
+            // --- 2. LOGIQUE ESPACE MODIFIÉE ---
             case ' ':
-                all_spots[selected_spot_index].is_occupied = !all_spots[selected_spot_index].is_occupied;
+                // Si la place est vide, on met le premier véhicule (Type 0)
+                if (!all_spots[selected_spot_index].is_occupied) {
+                    all_spots[selected_spot_index].is_occupied = 1;
+                    all_spots[selected_spot_index].type_vehicule = 0;
+                }
+                // Si elle est occupée, on passe au véhicule suivant
+                else {
+                    all_spots[selected_spot_index].type_vehicule++;
+                    
+                    // Si on a dépassé le dernier type (2), on vide la place
+                    if (all_spots[selected_spot_index].type_vehicule > 2) {
+                        all_spots[selected_spot_index].is_occupied = 0;
+                        all_spots[selected_spot_index].type_vehicule = 0; // Reset
+                    }
+                }
+                // On redessine immédiatement la place avec le nouveau véhicule
                 draw_spot(all_spots[selected_spot_index], 1);
                 break;
             }
+
             if (selection_changed)
             {
                 draw_spot(all_spots[old_selection], 0);
                 draw_spot(all_spots[selected_spot_index], 1);
             }
 
-            // CHANGEMENT: Afficher les messages plus bas
-            goto_xy(0, 15); // Anciennement 11
-            // printf("Place sélectionnée: %d   ", selected_spot_index);
+            goto_xy(0, 15); 
+            // Affichage de debug utile
+            printf("Place: %d | Type: %d   ", selected_spot_index, all_spots[selected_spot_index].type_vehicule);
             fflush(stdout);
         }
 
         usleep(50000);
     }
 
-    // CHANGEMENT: Nettoyer plus bas
-    goto_xy(0, 16); // Anciennement 12
+    goto_xy(0, 16); 
     printf("Simulation terminée.\n");
 
     return 0;
