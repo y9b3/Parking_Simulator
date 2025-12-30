7e0836400cfc100634626db0e1a0e25e7a460a87
Cass <cassandre.canvot@gmail.com>
2025-12-30 21:52:09 +0100

    Update parking.h

--- include/parking.h
+++ include/parking.h
@@ -1,22 +1,23 @@
 #ifndef PARKING_H
 #define PARKING_H
 
-#define TOTAL_SPOTS 42 // ici tu peux changer si tu change le nombre de P
+#define TOTAL_SPOTS 42 
 
-typedef struct
-{
+typedef struct {
     int screen_x;
     int screen_y;
     int is_occupied;
+    int type_vehicule; // <--- AJOUTE CETTE LIGNE OBLIGATOIREMENT
 } ParkingSpot;
 
 extern ParkingSpot all_spots[TOTAL_SPOTS];
 
+// Ajoute aussi le prototype de l'initialisation pour que le main le voie
+void init_modeles(void); 
+
 void display_static_map(const char *filename);
 void goto_xy(int x, int y);
-
 void init_spots_from_map(const char *filename);
-
 void draw_spot(ParkingSpot spot, int is_selected);
 void draw_all_spots(int selected_index);
 
