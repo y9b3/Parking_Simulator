ac4521ee3a1b46bb6eb114b0f1c683c0c0d00273
Cass <cassandre.canvot@gmail.com>
2026-01-04 16:59:11 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,133 +7,63 @@
 #include <time.h>
 #include "../include/parking.h"
 
-// --- FONCTION UTILITAIRE (Non-bloquante) ---
 char key_pressed() {
     struct termios oldterm, newterm;
-    int oldfd;
-    int c;
-    char result = 0;
+    int oldfd; int c; char result = 0;
     tcgetattr(STDIN_FILENO, &oldterm);
-    newterm = oldterm;
-    newterm.c_lflag &= ~(ICANON | ECHO);
+    newterm = oldterm; newterm.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newterm);
     oldfd = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, oldfd | O_NONBLOCK);
     c = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldterm);
     fcntl(STDIN_FILENO, F_SETFL, oldfd);
-    if (c != EOF) {
-        ungetc(c, stdin);
-        result = getchar();
-    }
+    if (c != EOF) { ungetc(c, stdin); result = getchar(); }
     return result;
 }
 
-// --- MENU DE DÉMARRAGE ---
 int afficher_menu() {
     int choix = 0;
-    while (choix != 1 && choix != 2 && choix != 3) {
-        printf("\033[2J\033[H"); 
-        printf("\n====================================\n");
-        printf("   PARKING SIMULATOR 2026 (ESIEA)   \n");
-        printf("====================================\n\n");
-        printf("1. Mode FLUIDE (Peu de trafic automatique)\n");
-        printf("2. Mode CHARGE (Gros trafic automatique)\n");
-        printf("3. Quitter\n\n");
-        printf("Votre choix : ");
-        if (scanf("%d", &choix) != 1) {
-            while(getchar() != '\n'); 
-        }
+    while (choix < 1 || choix > 3) {
+        printf("\033[2J\033[H\n====================================\n");
+        printf("   PARKING SIMULATOR 2026 (ESIEA)   \n====================================\n\n");
+        printf("1. Mode FLUIDE\n2. Mode CHARGE\n3. Quitter\n\nVotre choix : ");
+        if (scanf("%d", &choix) != 1) while (getchar() != '\n');
     }
     return choix;
 }
 
-// --- MAIN (Boucle Mixte : Auto + Manuel) ---
 int main() {
     srand(time(NULL));
+    while (1) {
+        int mode = afficher_menu();
+        if (mode == 3) break;
+        int chance = (mode == 1) ? 5 : 25;
 
-    int mode = afficher_menu();
-    if (mode == 3) return 0;
+        init_modeles();
+        init_spots_from_map("assets/parking_map.txt");
+        display_static_map("assets/parking_map.txt");
 
-    // Configuration de la probabilité de spawn automatique
-    int chance_spawn = (mode == 1) ? 5 : 25;
-
-    // Initialisation
-    init_modeles();
-    const char *map_path = "assets/parking_map.txt";
-    display_static_map(map_path);
-    init_spots_from_map(map_path);
-
-    int selected_spot_index = 0;
-    int timer_auto = 0;
-    char key = 0;
-
-    while (key != 'e') {
-        key = key_pressed();
-
-        // --- PARTIE A : TRAFIC AUTOMATIQUE (Code 1) ---
-        timer_auto++;
-        if (timer_auto > 10) { 
-            if ((rand() % 100) < chance_spawn) {
-                spawner_vehicule();
+        char key = 0;
+        int timer = 0;
+        while (key != 'e' && key != 'r') {
+            if (++timer > 10) {
+                if ((rand() % 100) < chance) spawner_vehicule();
+                timer = 0;
             }
-            timer_auto = 0;
+            mettre_a_jour_vehicules();
+            draw_all_spots(-1);
+            afficher_vehicules_dynamiques();
+            
+            goto_xy(0, 48);
+            printf("\033[KMODE: %s | 'r': Menu | 'e': Quitter", (mode == 1 ? "FLUIDE" : "CHARGE"));
+            fflush(stdout);
+            usleep(50000);
+            key = key_pressed();
         }
-
-        // --- PARTIE B : GESTION MANUELLE ZQSD (Code 2) ---
-        if (key != 0) {
-            int old_selection = selected_spot_index;
-            int selection_changed = 0;
-
-            switch (key) {
-                case 'q': case 'z': // Gauche ou Haut
-                    if (selected_spot_index > 0) {
-                        selected_spot_index--;
-                        selection_changed = 1;
-                    }
-                    break;
-                case 'd': case 's': // Droite ou Bas
-                    if (selected_spot_index < TOTAL_SPOTS - 1) {
-                        selected_spot_index++;
-                        selection_changed = 1;
-                    }
-                    break;
-                case ' ': // ESPACE : Changer manuellement le véhicule sur le spot
-                    if (!all_spots[selected_spot_index].is_occupied) {
-                        all_spots[selected_spot_index].is_occupied = 1;
-                        all_spots[selected_spot_index].type_vehicule = 0;
-                    } else {
-                        all_spots[selected_spot_index].type_vehicule++;
-                        if (all_spots[selected_spot_index].type_vehicule > 2) {
-                            all_spots[selected_spot_index].is_occupied = 0;
-                            all_spots[selected_spot_index].type_vehicule = 0;
-                        }
-                    }
-                    draw_spot(all_spots[selected_spot_index], 1);
-                    break;
-            }
-
-            if (selection_changed) {
-                draw_spot(all_spots[old_selection], 0);
-                draw_spot(all_spots[selected_spot_index], 1);
-            }
-        }
-
-        // --- PARTIE C : MISE À JOUR MOTEUR & AFFICHAGE ---
-        mettre_a_jour_vehicules(); // Moteur physique
-        draw_all_spots(selected_spot_index); // Dessine les places (avec curseur de sélection)
-        afficher_vehicules_dynamiques(); // Dessine les voitures qui roulent
-
-        // Status bar en bas
-        goto_xy(0, 38); 
-        printf("\033[KMODE: %s | SPOT: %d | ZQSD: Naviguer | ESPACE: Modifier | E: Quitter", 
-               (mode==1 ? "FLUIDE" : "CHARGE"), selected_spot_index);
-
-        fflush(stdout);
-        usleep(50000); 
+        liberer_memoire_vehicules();
+        if (key == 'e') break;
     }
-
-    liberer_memoire_vehicules();
     printf("\033[2J\033[HSimulation terminee.\n");
     return 0;
 }
